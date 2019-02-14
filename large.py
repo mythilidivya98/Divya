051366288619e2a3cfa69102fdeda67fd35f0f16
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-02-14 11:05:49 +0530

    Update large.py

--- large.py
+++ large.py
@@ -1,9 +1,9 @@
 def large(a,n)
-  max=a[0]
+  min=a[0]
   for i in range(1,n):
-      if a[i]>max:
-         max=a[i]
-  return max
+      if a[i]<min:
+         min=a[i]
+  return min
   a=[]
   n=len(a)
   ans=large(a,n)
