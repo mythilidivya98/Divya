20926c6618d79ec6236ccda9abc6b4456b352624
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-02-14 10:49:48 +0530

    Update lar.py

--- lar.py
+++ lar.py
@@ -1,9 +1,9 @@
 a=int(input())
 b=int(input())
 c=int(input())
-if((a>b)and(a>c)):
+if(a>b)and(a>c):
  print("A is GREATER")
-elif((b>a)and(b>c)):
+elif(b>a)and(b>c):
  print("B is GREATER")
 else:
  print("C is GREATER")
