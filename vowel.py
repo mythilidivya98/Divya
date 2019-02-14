73f96f189cd5fde208447bd189f922958ed08816
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-02-14 10:26:54 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,5 @@
 c=input()
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')||(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')):
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'):
   print("Vowels")
 else:
   print("Consonent")
