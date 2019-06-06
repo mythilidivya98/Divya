aff054bc835f4f38e97a5b75848104ecfc4c3f99
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-06-06 14:52:12 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,10 +1,3 @@
-def large(a,n)
-  max=a[0]
-  for i in range(1,n):
-      if a[i]>max:
-         max=a[i]
-  return max
-a=[]
-n=len(a)
-ans=large(a,n)
-print(ans)
+a=int(input())
+a1=list(input().split())
+print(max(a1))
