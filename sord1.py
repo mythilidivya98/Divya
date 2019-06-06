08c762b8f8c44f4281597a79ff171268981b67ec
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-06-06 15:39:42 +0530

    Update sord1.py

--- sord1.py
+++ sord1.py
@@ -1,10 +1,10 @@
 r21=int(input())
 a=list(input().split())
 b11=0
-for i in range(r1):
+for i in range(r21):
     if(int(a[i])>0):
         b=sorted(a)
-        b1=1
+        b11=1
     else:
         c=reversed(a)
 if(b11==1):
