899c82072aa67ef16354b4fe227a2e695df4c019
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-06-06 15:15:16 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,13 @@
-a2=int(input())
-a1=list(input().split())
-b=sorted(a1)
-print(' '.join(b))
+r1=int(input())
+a=list(input().split())
+b1=0
+for i in range(r1):
+    if(int(a[i])>0):
+        b=sorted(a)
+        b1=1
+    else:
+        c=reversed(a)
+if(b1==1):
+    print(' '.join(b))
+else:
+    print(' '.join(c))
