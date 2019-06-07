b8dd727a3ac65f9a003986797e1e6a9f28e4121d
mythilidivya98 <46745318+mythilidivya98@users.noreply.github.com>
2019-06-07 16:12:16 +0530

    Update unique.py

--- unique.py
+++ unique.py
@@ -1,10 +1,13 @@
-n=int(input())
+n1=int(input())
 n2=list(input().split())
-f=[]
+e=[]
 #c=0
-for i in range(n):
-    for j in range(i+1,n):
-        if (n2[i]==n2[j] and n2[i] not in f):
+for i in range(n1):
+    for j in range(i+1,n1):
+        if (n2[i]==n2[j] and n2[i] not in e):
             k=n2[i]
-            f.append(k)
-print(' '.join(f))
+            e.append(k)
+            break
+else:
+    print("unique")
+print(' '.join(e))
